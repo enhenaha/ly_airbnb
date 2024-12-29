fef0af1975f46044e319094abaca634789044347
lyp <2237962884@qq.com>
2024-12-29 16:15:55 +0800

    02_完成了首页header部分

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,39 +1,9 @@
-import React, { memo, useEffect, useState } from 'react'
-import hyRequest from '@/services'
+import React, { memo } from 'react'
 
 const Home = memo(() => {
-  // 定义状态
-  const [ highScore, setHighScore ] = useState({})
-
-  // 网络请求的代码
-  useEffect(() => {
-    hyRequest.get({ url: '/home/highscore' }).then(res => {
-      console.log(res)
-      setHighScore(res)
-    })
-  }, [])
-
   return (
     <div>
-      <h2>{highScore.title}</h2>
-      <h4>{highScore.subtitle}</h4>
-      {/* {
-        highScore.list && <ul>
-          {
-            highScore.list.map(item => {
-              return <li key={item.id}>{item.name}</li>
-            })
-          }
-        </ul>
-      } */}
-      <ul>
-        {
-          highScore.list?.map(item => {
-            return <li key={item.id}>{item.name}</li>
-          })
-        }
-      </ul>
-      
+      <h2>Home Page</h2>
     </div>
   )
 })
